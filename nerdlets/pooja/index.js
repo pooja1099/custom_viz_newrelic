8d09ca9f839fe36f365f51b137c4a99e23e57b6d
pooja1099 <124161579+pooja1099@users.noreply.github.com>
2023-09-22 15:44:48 +0530

    Update index.js

--- nerdlets/pooja/index.js
+++ nerdlets/pooja/index.js
@@ -18,7 +18,7 @@ export default class PoojaNerdlet extends React.Component {
     return <>
     <p style={{color: "red", padding:"10px", alignitem:"center", background:"yellow", fontSize:"20px", textAlign:"center"}}>This is LineChart</p>
     <LineChart style={{width:"90%", height:"95%"}}
-    accountId = {3930388}
+    accountId = {*******}
     query = "SELECT average(host.net.transmitBytesPerSecond) AS 'Transmit bytes per second', average(host.net.receiveBytesPerSecond) AS 'Receive bytes per second' FROM Metric WHERE `entityGuid` = 'MzkzMDM4OHxJTkZSQXxOQXw0NTkyMDE1NTk3OTU1OTIwNTQ2' TIMESERIES auto"
     fullWidth
     fullHeight
@@ -26,7 +26,7 @@ export default class PoojaNerdlet extends React.Component {
     
     <p style={{color: "red", padding:"10px" , alignitem:"center", background:"yellow", fontSize:"20px", textAlign:"center"}}>This is Pie-Chart</p>
     <PieChart
-    accountId = {3930388}
+    accountId = {*******}
     query = "SELECT average(host.net.transmitBytesPerSecond) AS 'Transmit bytes per second', average(host.net.receiveBytesPerSecond) AS 'Receive bytes per second' FROM Metric WHERE `entityGuid` = 'MzkzMDM4OHxJTkZSQXxOQXw0NTkyMDE1NTk3OTU1OTIwNTQ2' TIMESERIES auto"
     fullWidth
     fullHeight
